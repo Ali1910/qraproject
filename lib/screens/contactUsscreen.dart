69ca966b789b0c0db2f4f02d4c891204463d69a0
Ali1910 <99036660+Ali1910@users.noreply.github.com>
2023-02-18 11:21:04 +0200

    Add files via upload

---
+++ lib/screens/contactUsscreen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: ContactUsPage(),
+    ),
+  );
+}
+
+class ContactUsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Contact Us'),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _buildButton('Customer Service', Icons.headset_mic),
+            SizedBox(height: 20),
+            _buildButton('WhatsApp', Icons.phone),
+            SizedBox(height: 20),
+            _buildButton('Website', Icons.public),
+            SizedBox(height: 20),
+            _buildButton('Facebook', Icons.facebook),
+            SizedBox(height: 20),
+            _buildButton('Instagram', Icons.camera_alt),
+            SizedBox(height: 20),
+            _buildButton('location', Icons.location_on),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButton(String title, IconData icon) {
+    return SizedBox(
+      width: 250,
+      height: 50,
+      child: ElevatedButton.icon(
+        icon: Icon(icon, color: Colors.green),
+        label: Text(title, style: TextStyle(fontSize: 18)),
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          primary: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+        ),
+      ),
+    );
+  }
+}
